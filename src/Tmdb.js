3792a96a100be1cd2ecd863b9f07dc464c5ddddb
Maysa Pereira <maysalvespereira@gmail.com>
2022-03-27 22:17:21 -0300

    tive que mudar os generos porque alguns que eu tinha criado antes nao existiam

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,5 +1,12 @@
 const API_KEY = 'd2c520c5aaf43f4f73a356f18c19a5b2'
 const API_BASE = 'https://api.themoviedb.org/3'
+const language = 'language=pt-BR'
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json()
+    return json
+}
 
 export default {
     getHomeList: async () => {
@@ -7,49 +14,49 @@ export default {
             {
                 slug: 'originais',
                 title: 'Originais Netflix',
-                items: []
+                items: await basicFetch(`/discover/tv?with_network=213&${language}&api_key=${API_KEY}`)
             },
 
             {
                 slug: 'trending',
-                title: 'Recomendados para você'
-                items: []
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?${language}&api_key=${API_KEY}`)
             },
 
             {
-                slug: 'toprated',
+                slug: 'top-rated',
                 title: 'Em Alta',
-                items: []
+                items: await basicFetch(`/tv/top_rated?${language}&api_key=${API_KEY}`)
             },
 
             {
-                slug: 'kdramas',
-                title: 'K-dramas',
-                items: []
+                slug: 'crime',
+                title: 'Criminal',
+                items: await basicFetch(`/discover/tv?with_genres=&80${language}&api_key=${API_KEY}`)
             },
 
             {
-                slug: 'suspenseful',
-                tittle: 'Suspense',
-                items: []
+                slug: 'mystery',
+                tittle: 'Mistério',
+                items: await basicFetch(`/discover/tv?with_genres=&9648${language}&api_key=${API_KEY}`)
             },
 
             {
-                slug: 'supernatural',
-                tittle: 'Sobrenatural',
-                items: []
+                slug: 'horror',
+                tittle: 'Terror',
+                items: await basicFetch(`/discover/tv?with_genres=&27${language}&api_key=${API_KEY}`)
             },
 
             {
                 slug: 'sci-fi',
                 tittle: 'Sci-fi',
-                items: []
+                items: await basicFetch(`/discover/tv?with_genres=&878${language}&api_key=${API_KEY}`)
             },
 
             {
                 slug: 'romance',
                 tittle: 'Romance',
-                items: []
+                items: await basicFetch(`/discover/tv?with_genres=&10749${language}&api_key=${API_KEY}`)
             }
         ]
     }
