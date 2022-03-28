d41d3f268bfb7576b9ec06bf08a8bc0894954aa8
Maysa Pereira <maysalvespereira@gmail.com>
2022-03-27 23:28:44 -0300

    posicionamento das imagens

--- src/components/TVRow.js
+++ src/components/TVRow.js
@@ -3,13 +3,24 @@ import './TVRow.css'
 
 export default ({title, items}) => {
     return (
-        <div>
+        <div className='tvRow'>
             <h2>{title}</h2>
             <div className='tvRow--listarea'>
+
+                <div className='tvRow--list'>
+
                 {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} 
-                    alt={item.original_title}/>
+
+                    <div key={key} className='tvRow--item'>
+
+                        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} 
+                        alt={item.original_title}/>
+
+                    </div>
                 ))}
+
+                </div>
+
             </div>
         </div>
     )
